f2777ba60fb1479ed9af11ff41a5c52b2d45d48c
marcealban <57547734+marcealban@users.noreply.github.com>
2025-09-18 23:22:09 -0500

    Handle app label extraction asynchronously

--- preload.js
+++ preload.js
@@ -5,5 +5,17 @@ contextBridge.exposeInMainWorld('launcher', {
   minimize: () => ipcRenderer.invoke('minimize'),
   connect: () => ipcRenderer.invoke('connect'),
   listPackages: () => ipcRenderer.invoke('list-packages'),
-  launchApp: (pkg) => ipcRenderer.invoke('launch-app', pkg)
+  launchApp: (pkg) => ipcRenderer.invoke('launch-app', pkg),
+  onPackageLabelUpdated: (callback) => {
+    if (typeof callback !== 'function') return () => {};
+    const subscription = (_event, payload) => callback(payload);
+    ipcRenderer.on('package-label-updated', subscription);
+    return () => ipcRenderer.removeListener('package-label-updated', subscription);
+  },
+  onPackageLabelStarted: (callback) => {
+    if (typeof callback !== 'function') return () => {};
+    const subscription = (_event, pkg) => callback(pkg);
+    ipcRenderer.on('package-label-started', subscription);
+    return () => ipcRenderer.removeListener('package-label-started', subscription);
+  }
 });
